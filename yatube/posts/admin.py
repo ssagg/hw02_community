4a54dde51a259d480ea7e8a37a6ae2529bdd84b3
Alexander Sorokin <personal-photos@yandex.ru>
2022-08-22 22:21:24 +0300

    review 1: ne template and Meta class

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -14,8 +14,5 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
-# При регистрации модели Post источником конфигурации для неё назначаем
-# класс PostAdmin
 admin.site.register(Post, PostAdmin)
-# Register your models here.
 admin.site.register(Group)
